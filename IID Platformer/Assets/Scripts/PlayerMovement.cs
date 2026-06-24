96c1f857ca7329acd317f72acc3dedcf667130df
ISARTiNeedThatInternship <angus.capitaine@gmail.com>
2026-06-24 22:08:52 +0200

    Changements variés dans 'PlayerMovement.cs' et ajout d'un spritesheet de pièce 🪙

--- IID Platformer/Assets/Scripts/PlayerMovement.cs
+++ IID Platformer/Assets/Scripts/PlayerMovement.cs
@@ -2,34 +2,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 250;
-    public float jumpForce = 300; 
-    public bool isJumping;
-    public bool isGrounded;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private bool isJumping;
+    private bool isGrounded;
 
     public Transform groundCheck;
     public float groundCheckRadius;
     public LayerMask collisionLayers;
+
+    public Rigidbody2D rb;
     public Animator animator;
-    
-    public Rigidbody2D rb; 
     public SpriteRenderer spriteRenderer;
+
     private Vector3 velocity = Vector3.zero;
-    private float horizontalInput;
     private float horizontalMovement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-    }
 
+    }
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
 
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
@@ -37,26 +36,22 @@ public class PlayerMovement : MonoBehaviour
         }
 
         Flip(rb.linearVelocityX);
+
+        float characterVelocity = Mathf.Abs(rb.linearVelocityX);
+        animator.SetFloat("Speed", characterVelocity);
     }
 
     void FixedUpdate()
     {
-        float horizontalMovement = horizontalInput * moveSpeed * Time.deltaTime;
-
         MovePlayer(horizontalMovement);
-
-        float characterVelocity = Mathf.Abs(rb.linearVelocityX);// Permet d'avoir une vitesse positive en allant vers la gauche
-        animator.SetFloat("Speed", characterVelocity);
-
-        Flip(rb.linearVelocity.x);
     }
 
     void MovePlayer(float _horizontalMovement)
     {
-        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocity.y);
-        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, 0.1f);
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.linearVelocityY);
+        rb.linearVelocity = Vector3.SmoothDamp(rb.linearVelocity, targetVelocity, ref velocity, .05f);
 
-        if (isJumping == true)
+        if(isJumping == true)
         {
             rb.AddForce(new Vector2(0f, jumpForce));
             isJumping = false;
@@ -68,14 +63,13 @@ public class PlayerMovement : MonoBehaviour
         if (_velocity > 0.1f)
         {
             spriteRenderer.flipX = false;
-        }
-        else if (_velocity < -0.1f)
+        }else if(_velocity < -0.1f)
         {
             spriteRenderer.flipX = true;
         }
     }
 
-    void OnDrawGizmos()
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
