f5df027232ad90db0bea6d8015cdbce4e53c0fb6
ISARTiNeedThatInternship <angus.capitaine@gmail.com>
2026-06-27 22:16:20 +0200

    Ajout d'une zone de mort avec le même fondu que le changement de niveau

--- IID Platformer/Assets/Scripts/DontDestroyOnLoadScene.cs
+++ IID Platformer/Assets/Scripts/DontDestroyOnLoadScene.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class DontDestroyOnLoadScene : MonoBehaviour
 {
-    public GameObject[] objects;
+    public GameObject[] objects; // Crée un array d'objets à ne PAS suppprimer
     void Awake()
     {
-        foreach (var element in objects)
+        foreach (var element in objects) // Applique une condition pour tous les objets de la liste
         {
-            DontDestroyOnLoad(element);
+            DontDestroyOnLoad(element); // Dit à unity de ne pas supprimer ces éléments
         }
     }
 }
